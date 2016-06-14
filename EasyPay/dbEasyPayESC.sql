6f494f77c3c0442dc77105172b057975e2d8737a
armandoomtzz <armandoomtzz@gmail.com>
2016-06-14 08:40:29 -0500

    Se creo el script de la base de datos

---
+++ EasyPay/dbEasyPayESC.sql
@@ -0,0 +1,11 @@
+﻿/*
+Nombre script: dbEasyPayESC
+Descripcion: Creacion del script de la base de datos
+Version: 1.0
+
+Historia de modificacion: IS       Fecha                     Descripcion
+                          AGMM     14 de Junio del 2016      Se creo el script inicial
+*/
+
+use master
+go
