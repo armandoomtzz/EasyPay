743f1765f5bfc2f5abcd8426fbb39031020c99be
armandoomtzz <armandoomtzz@gmail.com>
2016-06-27 00:20:45 -0500

    Se agrego el formulario "frmVenta" faltante del proyecto.

---
+++ EasyPay/EasyPayWinFormApp/frmVenta.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace EasyPayWinFormApp
+{
+    partial class frmVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lblSlogan = new System.Windows.Forms.Label();
+            this.lblProductos = new System.Windows.Forms.Label();
+            this.txtProductos = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.txtBarras = new System.Windows.Forms.TextBox();
+            this.btnSesion = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(73, 120);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 0;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(542, 120);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 1;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(428, 328);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(545, 146);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 328);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // lblSlogan
+            // 
+            this.lblSlogan.AutoSize = true;
+            this.lblSlogan.Location = new System.Drawing.Point(626, 44);
+            this.lblSlogan.Name = "lblSlogan";
+            this.lblSlogan.Size = new System.Drawing.Size(131, 13);
+            this.lblSlogan.TabIndex = 4;
+            this.lblSlogan.Text = "Rapido y Eficaz, EasyPay.";
+            // 
+            // lblProductos
+            // 
+            this.lblProductos.AutoSize = true;
+            this.lblProductos.Location = new System.Drawing.Point(542, 520);
+            this.lblProductos.Name = "lblProductos";
+            this.lblProductos.Size = new System.Drawing.Size(97, 13);
+            this.lblProductos.TabIndex = 5;
+            this.lblProductos.Text = "Total de Productos";
+            // 
+            // txtProductos
+            // 
+            this.txtProductos.Location = new System.Drawing.Point(657, 517);
+            this.txtProductos.Name = "txtProductos";
+            this.txtProductos.Size = new System.Drawing.Size(128, 20);
+            this.txtProductos.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 517);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(73, 520);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(46, 13);
+            this.lblSubtotal.TabIndex = 7;
+            this.lblSubtotal.Text = "Subtotal";
+            this.lblSubtotal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBarras
+            // 
+            this.txtBarras.Location = new System.Drawing.Point(76, 588);
+            this.txtBarras.Name = "txtBarras";
+            this.txtBarras.Size = new System.Drawing.Size(709, 20);
+            this.txtBarras.TabIndex = 9;
+            this.txtBarras.Text = "Codigo de Barras";
+            // 
+            // btnSesion
+            // 
+            this.btnSesion.Location = new System.Drawing.Point(623, 697);
+            this.btnSesion.Name = "btnSesion";
+            this.btnSesion.Size = new System.Drawing.Size(162, 23);
+            this.btnSesion.TabIndex = 10;
+            this.btnSesion.Text = "Cerrar Sesion";
+            this.btnSesion.UseVisualStyleBackColor = true;
+            // 
+            // frmVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 741);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSesion);
+            this.Controls.Add(this.txtBarras);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.txtProductos);
+            this.Controls.Add(this.lblProductos);
+            this.Controls.Add(this.lblSlogan);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblDescripcion);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmVenta";
+            this.Text = "EasyPay";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label lblSlogan;
+        private System.Windows.Forms.Label lblProductos;
+        private System.Windows.Forms.TextBox txtProductos;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.TextBox txtBarras;
+        private System.Windows.Forms.Button btnSesion;
+    }
+}
